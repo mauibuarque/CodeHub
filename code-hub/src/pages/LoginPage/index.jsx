dfbd22549619007514d98c3c71cc4190871393a7
Mauí Buarque <maui.buarque1@gmail.com>
2024-03-11 22:09:58 -0300

    feat: loginPage

--- code-hub/src/pages/LoginPage/index.jsx
+++ code-hub/src/pages/LoginPage/index.jsx
@@ -1,7 +1,32 @@
+import { Button, Header, Input } from "../../components";
+
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
+  const handleLogin = () => {};
+
   return (
     <>
-      <h1>Login</h1>
+      <Header />
+
+      <form>
+        <h2>Login</h2>
+
+        <Input label="Email" type="text" placeholder="Digite aqui seu email" />
+
+        <Input
+          label="Senha"
+          type="password"
+          placeholder="Digite aqui sua senha"
+        />
+
+        <Button buttonValue="Entrar" onClickFunction={handleLogin} />
+
+        <div>
+          <p>Ainda não possui uma conta?</p>
+          <Link to="/register">Cadastre-se</Link>
+        </div>
+      </form>
     </>
   );
 };
